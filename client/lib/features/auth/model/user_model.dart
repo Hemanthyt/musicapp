f62040043cea34ee9dda553add102845023dfbbd
Hemanthyt <hemanth25052004@gmail.com>
2024-06-16 00:51:47 +0530

    added token to the user model

--- client/lib/features/auth/model/user_model.dart
+++ client/lib/features/auth/model/user_model.dart
@@ -5,22 +5,26 @@ class UserModel {
   final String name;
   final String email;
   final String password;
+  final String token;
 
   UserModel({
     required this.name,
     required this.email,
     required this.password,
+    required this.token,
   });
 
   UserModel copyWith({
     String? name,
     String? email,
     String? password,
+    String? token,
   }) {
     return UserModel(
       name: name ?? this.name,
       email: email ?? this.email,
       password: password ?? this.password,
+      token: token ?? this.token,
     );
   }
 
@@ -29,6 +33,7 @@ class UserModel {
       'name': name,
       'email': email,
       'password': password,
+      'token': token,
     };
   }
 
@@ -37,6 +42,7 @@ class UserModel {
       name: map['name'] ?? '',
       email: map['email'] ?? '',
       password: map['password'] ?? '',
+      token: map['token'] ?? '',
     );
   }
 
@@ -46,18 +52,26 @@ class UserModel {
       UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() =>
-      'UserModel(name: $name, email: $email, password: $password)';
+  String toString() {
+    return 'UserModel(name: $name, email: $email, password: $password, token: $token)';
+  }
 
   @override
   bool operator ==(covariant UserModel other) {
     if (identical(this, other)) return true;
-
-    return other.name == name &&
-        other.email == email &&
-        other.password == password;
+  
+    return 
+      other.name == name &&
+      other.email == email &&
+      other.password == password &&
+      other.token == token;
   }
 
   @override
-  int get hashCode => name.hashCode ^ email.hashCode ^ password.hashCode;
+  int get hashCode {
+    return name.hashCode ^
+      email.hashCode ^
+      password.hashCode ^
+      token.hashCode;
+  }
 }
