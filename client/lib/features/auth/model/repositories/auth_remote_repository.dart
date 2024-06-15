2fdf451878d15221a15a78b14dc957aabb156d28
Hemanthyt <hemanth25052004@gmail.com>
2024-06-16 00:53:21 +0530

    added currentuser data

--- client/lib/features/auth/model/repositories/auth_remote_repository.dart
+++ client/lib/features/auth/model/repositories/auth_remote_repository.dart
@@ -4,6 +4,14 @@ import 'package:client/core/failure/failure.dart';
 import 'package:client/features/auth/model/user_model.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'auth_remote_repository.g.dart';
+
+@riverpod
+AuthRemoteRepository authRemoteRepository(AuthRemoteRepositoryRef ref) {
+  return AuthRemoteRepository();
+}
 
 class AuthRemoteRepository {
   Future<Either<AppFailure, UserModel>> signup({
@@ -65,7 +73,34 @@ class AuthRemoteRepository {
         return Left(AppFailure(resBodyMap['detail']));
       }
 
-      return Right(UserModel.fromMap(resBodyMap));
+      return Right(UserModel.fromMap(resBodyMap["user"]).copyWith(
+        token: resBodyMap["token"],
+      ));
+    } catch (e) {
+      return Left(AppFailure(e.toString()));
+    }
+  }
+
+  Future<Either<AppFailure, UserModel>> CurrentUserData(String token) async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+          '${ServerConstant.serverURL}/auth/',
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-auth-token':token,
+        },
+      );
+      final resBodyMap = jsonDecode(response.body) as Map<String, dynamic>;
+
+      if (response.statusCode != 200) {
+        return Left(AppFailure(resBodyMap['detail']));
+      }
+
+      return Right(UserModel.fromMap(resBodyMap).copyWith(
+        token: token,
+      ));
     } catch (e) {
       return Left(AppFailure(e.toString()));
     }
