1651333e9e35eead68463429735bab3ad2c6323e
Hemanthyt <hemanth25052004@gmail.com>
2024-06-17 00:22:29 +0530

    upload song page done with audiowaveform used there

--- client/lib/features/home/view/pages/upload_song_page.dart
+++ client/lib/features/home/view/pages/upload_song_page.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:client/core/theme/app_pallate.dart';
 import 'package:client/core/utils.dart';
 import 'package:client/core/widgets/custom_field.dart';
+import 'package:client/core/widgets/loader.dart';
 import 'package:client/features/home/view/widgets/audio_wave.dart';
+import 'package:client/features/home/viewmodel/home_viewmodel.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +25,7 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
   Color selectedColor = Pallete.cardColor;
   File? selectedImage;
   File? selectedAudio;
+  final formKey = GlobalKey<FormState>();
 
   void selectAudio() async {
     final pickedAudio = await pickAudio();
@@ -51,108 +55,134 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref
+        .watch(homeViewmodelProvider.select((val) => val?.isLoading == true));
     return Scaffold(
       appBar: AppBar(
         title: const Text('Upload Song'),
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () async {
+              if (formKey.currentState!.validate() &&
+                  selectedAudio != null &&
+                  selectedImage != null) {
+                ref.watch(homeViewmodelProvider.notifier).uploadSong(
+                      selectedAudio: selectedAudio!,
+                      selectedThumbnail: selectedImage!,
+                      songName: songNameController.text,
+                      artist: artistNameController.text,
+                      selectedColor: selectedColor,
+                    );
+              } else {
+                showSnackBar(
+                  context,
+                  'Error',
+                  'Missing Fields!.',
+                  ContentType.failure,
+                );
+              }
+            },
             icon: const Icon(Icons.check),
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              GestureDetector(
-                // https://blog.wavezoo.com/wp-content/uploads/2022/03/top_female_djs_world.webp
-                onTap: selectImage,
-                child: selectedImage != null
-                    ? SizedBox(
-                        height: 180,
-                        width: double.infinity,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.file(
-                            selectedImage!,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      )
-                    : DottedBorder(
-                        color: Pallete.borderColor,
-                        dashPattern: const [10, 4],
-                        radius: const Radius.circular(10),
-                        borderType: BorderType.RRect,
-                        strokeCap: StrokeCap.round,
-                        child: const SizedBox(
-                          height: 180,
-                          width: double.infinity,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.folder_open,
-                                size: 40,
-                              ),
-                              SizedBox(
-                                height: 15,
-                              ),
-                              Text(
-                                'Select the thumbnail for your song',
+      body: isLoading
+          ? const Loader()
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        // https://blog.wavezoo.com/wp-content/uploads/2022/03/top_female_djs_world.webp
+                        onTap: selectImage,
+                        child: selectedImage != null
+                            ? SizedBox(
+                                height: 180,
+                                width: double.infinity,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.file(
+                                    selectedImage!,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
                               )
-                            ],
-                          ),
-                        ),
+                            : DottedBorder(
+                                color: Pallete.borderColor,
+                                dashPattern: const [10, 4],
+                                radius: const Radius.circular(10),
+                                borderType: BorderType.RRect,
+                                strokeCap: StrokeCap.round,
+                                child: const SizedBox(
+                                  height: 180,
+                                  width: double.infinity,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.folder_open,
+                                        size: 40,
+                                      ),
+                                      SizedBox(
+                                        height: 15,
+                                      ),
+                                      Text(
+                                        'Select the thumbnail for your song',
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
                       ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      selectedAudio != null
+                          ? AudioWave(path: selectedAudio!.path)
+                          : CustomField(
+                              hintText: 'Pick Song',
+                              controller: null,
+                              readOnly: true,
+                              onTap: selectAudio,
+                              suffixIcon: const Icon(Icons.music_note),
+                            ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomField(
+                        hintText: 'Artist',
+                        controller: artistNameController,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomField(
+                        hintText: 'Song Name',
+                        controller: songNameController,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ColorPicker(
+                        pickersEnabled: const {
+                          ColorPickerType.wheel: true,
+                        },
+                        color: selectedColor,
+                        onColorChanged: (Color color) {
+                          setState(() {
+                            selectedColor = color;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              const SizedBox(
-                height: 40,
-              ),
-              selectedAudio != null
-                  ? AudioWave(path: selectedAudio!.path)
-                  : CustomField(
-                      hintText: 'Pick Song',
-                      controller: null,
-                      readOnly: true,
-                      onTap: selectAudio,
-                      suffixIcon: Icon(Icons.music_note),
-                    ),
-              const SizedBox(
-                height: 20,
-              ),
-              const CustomField(
-                hintText: 'Artist',
-                controller: null,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const CustomField(
-                hintText: 'Song Name',
-                controller: null,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ColorPicker(
-                pickersEnabled: const {
-                  ColorPickerType.wheel: true,
-                },
-                color: selectedColor,
-                onColorChanged: (Color color) {
-                  setState(() {
-                    selectedColor = color;
-                  });
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
