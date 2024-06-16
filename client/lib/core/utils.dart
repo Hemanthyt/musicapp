eb5c3ce3fe6b6b52ebb5032313d2d889b3ff495b
Hemanthyt <hemanth25052004@gmail.com>
2024-06-17 00:19:04 +0530

    added hextocolor and rgbtohex function

--- client/lib/core/utils.dart
+++ client/lib/core/utils.dart
@@ -6,6 +6,14 @@ import 'package:client/core/widgets/snackbar.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
+String rgbToHex(Color color) {
+  return '${color.red.toRadixString(16).padLeft(2, '0')}${color.green.toRadixString(16).padLeft(2, '0')}${color.blue.toRadixString(16).padLeft(2, '0')}';
+}
+
+Color hexToColor(String hex) {
+  return Color(int.parse(hex, radix: 16) + 0xFF000000);
+}
+
 void showSnackBar(BuildContext context, String title, String message,
     ContentType contentType) {
   ScaffoldMessenger.of(context)
@@ -19,30 +27,31 @@ void showSnackBar(BuildContext context, String title, String message,
       ),
     );
 }
-Future<File?> pickImage() async {
-    try {
-      final filePickerRes = await FilePicker.platform.pickFiles(
-        type: FileType.image,
-      );
-      if (filePickerRes != null) {
-        return File(filePickerRes.files.first.xFile.path);
-      }
-      return null;
-    } catch (e) {
-      return null;
-    }
-  }
 
-  Future<File?> pickAudio() async {
-    try {
-      final filePickerRes = await FilePicker.platform.pickFiles(
-        type: FileType.audio,
-      );
-      if (filePickerRes != null) {
-        return File(filePickerRes.files.first.xFile.path);
-      }
-      return null;
-    } catch (e) {
-      return null;
+Future<File?> pickImage() async {
+  try {
+    final filePickerRes = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+    );
+    if (filePickerRes != null) {
+      return File(filePickerRes.files.first.xFile.path);
     }
+    return null;
+  } catch (e) {
+    return null;
   }
+}
+
+Future<File?> pickAudio() async {
+  try {
+    final filePickerRes = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+    );
+    if (filePickerRes != null) {
+      return File(filePickerRes.files.first.xFile.path);
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
