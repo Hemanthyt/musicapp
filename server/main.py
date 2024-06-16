25f4f4d187f7f56a3227e3e052a7f73a25ebb26d
Hemanthyt <hemanth25052004@gmail.com>
2024-06-17 00:23:26 +0530

    added song router

--- server/main.py
+++ server/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from models.base import Base
-from routes import auth
+from routes import auth,song
 from database import engine
 import uvicorn
 
 
 app = FastAPI()
 app.include_router(auth.router,prefix='/auth')
+app.include_router(song.router,prefix='/song')
 
 
 # Based on the this sqlalchemy will create a table
