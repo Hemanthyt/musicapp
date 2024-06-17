17381a7346828f6ac7c01a0c5bf16290de1e8893
Hemanthyt <hemanth25052004@gmail.com>
2024-06-18 01:26:25 +0530

    new card design created and used and called currentsonnotifiwe and play songs

--- client/lib/features/home/view/pages/songs_page.dart
+++ client/lib/features/home/view/pages/songs_page.dart
@@ -1,6 +1,8 @@
+import 'package:client/core/providers/current_song_notifier.dart';
 import 'package:client/core/theme/app_pallate.dart';
 import 'package:client/core/widgets/loader.dart';
 import 'package:client/features/home/repositories/home_repository.dart';
+import 'package:client/features/home/view/widgets/song_image_card.dart';
 import 'package:client/features/home/viewmodel/home_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -32,53 +34,23 @@ class SongPage extends ConsumerWidget {
                       scrollDirection: Axis.horizontal,
                       itemCount: songs.length,
                       itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(left: 16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                width: 180,
-                                height: 180,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: NetworkImage(
-                                      songs[index].thumbnail_url,
-                                    ),
-                                    fit: BoxFit.cover,
-                                  ),
-                                  borderRadius: BorderRadius.circular(8),
+                        final song = songs[index];
+                        return GestureDetector(
+                          onTap: () {
+                            ref
+                                .read(currentSongNotifierProvider.notifier)
+                                .updateSong(song);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SongCard(
+                                  song: song,
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 6,
-                              ),
-                              SizedBox(
-                                width: 180,
-                                child: Text(
-                                  songs[index].song_name,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w700,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  maxLines: 1,
-                                ),
-                              ),
-                              SizedBox(
-                                width: 180,
-                                child: Text(
-                                  songs[index].artist,
-                                  style: const TextStyle(
-                                    color: Pallete.subtitleText,
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w500,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  maxLines: 1,
-                                ),
-                              )
-                            ],
+                              ],
+                            ),
                           ),
                         );
                       }),
