7d6d182352f8805c548a27ade69151bdaf2f2649
Hemanthyt <hemanth25052004@gmail.com>
2024-06-19 03:16:33 +0530

    added favorites and getall fav from backend

--- server/routes/song.py
+++ server/routes/song.py
@@ -1,23 +1,24 @@
+import uuid
 from fastapi import APIRouter, Depends, File, Form, UploadFile
 from sqlalchemy.orm import Session
-
 from database import get_db
 from middleware.auth_middleware import auth_middleware
 import cloudinary
 import cloudinary.uploader
-import uuid
-
+from models.favorite import Favorite
 from models.song import Song
+from pydantic_schema.favorite_song import FavoriteSong
+from sqlalchemy.orm import joinedload
 
 router = APIRouter()
 
-# Configuration       
 cloudinary.config( 
-    cloud_name = "digtnt2rb", 
-    api_key = "853979898983621", 
-    api_secret = "ceVmY-d0N3j0ybcjH4VobOGWAW4", # Click 'View Credentials' below to copy your API secret
+    cloud_name = "dsomyc4hv", 
+    api_key = "433823972159394",
+    api_secret = "i7gUk6UOSuARwXHRe6Wz-6Uu7uo",
     secure=True
 )
+
 @router.post('/upload', status_code=201)
 def upload_song(song: UploadFile = File(...), 
                 thumbnail: UploadFile = File(...), 
@@ -26,7 +27,6 @@ def upload_song(song: UploadFile = File(...),
                 hex_code: str = Form(...),
                 db: Session = Depends(get_db),
                 auth_dict = Depends(auth_middleware)):
-    
     song_id = str(uuid.uuid4())
     song_res = cloudinary.uploader.upload(song.file, resource_type='auto', folder=f'songs/{song_id}')
     thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type='image', folder=f'songs/{song_id}')
@@ -46,7 +46,34 @@ def upload_song(song: UploadFile = File(...),
     return new_song
 
 @router.get('/list')
-def list_songs(db:Session=Depends(get_db),auth_details = Depends(auth_middleware)):
+def list_songs(db: Session=Depends(get_db), 
+               auth_details=Depends(auth_middleware)):
     songs = db.query(Song).all()
     return songs
+
+@router.post('/favorite')
+def favorite_song(song: FavoriteSong, 
+                  db: Session=Depends(get_db), 
+                  auth_details=Depends(auth_middleware)):
+    # song is already favorited by the user
+    user_id = auth_details['uid']
+
+    fav_song = db.query(Favorite).filter(Favorite.song_id == song.song_id, Favorite.user_id == user_id).first()
+
+    if fav_song:
+        db.delete(fav_song)
+        db.commit()
+        return {'message': False}
+    else:
+        new_fav = Favorite(id=str(uuid.uuid4()), song_id=song.song_id, user_id=user_id)
+        db.add(new_fav)
+        db.commit()
+        return {'message': True}
     
+@router.get('/list/favorites')
+def list_fav_songs(db: Session=Depends(get_db), 
+               auth_details=Depends(auth_middleware)):
+    user_id = auth_details['uid']
+    fav_songs = db.query(Favorite).filter(Favorite.user_id == user_id).options(joinedload(Favorite.song)).all()
+    
+    return fav_songs 
