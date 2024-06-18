e2856516b797e24e56979a941442ed3257503de3
Hemanthyt <hemanth25052004@gmail.com>
2024-06-19 03:10:25 +0530

    added favorites in usermodel

--- client/lib/features/auth/model/user_model.dart
+++ client/lib/features/auth/model/user_model.dart
@@ -1,30 +1,36 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+import 'package:client/features/home/model/fav_song_model.dart';
+import 'package:flutter/foundation.dart';
+
 class UserModel {
   final String name;
   final String email;
-  final String password;
+  final String id;
   final String token;
-
+  final List<FavSongModel> favorites;
   UserModel({
     required this.name,
     required this.email,
-    required this.password,
+    required this.id,
     required this.token,
+    required this.favorites,
   });
 
   UserModel copyWith({
     String? name,
     String? email,
-    String? password,
+    String? id,
     String? token,
+    List<FavSongModel>? favorites,
   }) {
     return UserModel(
       name: name ?? this.name,
       email: email ?? this.email,
-      password: password ?? this.password,
+      id: id ?? this.id,
       token: token ?? this.token,
+      favorites: favorites ?? this.favorites,
     );
   }
 
@@ -32,8 +38,9 @@ class UserModel {
     return <String, dynamic>{
       'name': name,
       'email': email,
-      'password': password,
+      'id': id,
       'token': token,
+      'favorites': favorites.map((x) => x.toMap()).toList(),
     };
   }
 
@@ -41,8 +48,13 @@ class UserModel {
     return UserModel(
       name: map['name'] ?? '',
       email: map['email'] ?? '',
-      password: map['password'] ?? '',
+      id: map['id'] ?? '',
       token: map['token'] ?? '',
+      favorites: List<FavSongModel>.from(
+        (map['favorites'] ?? []).map(
+          (x) => FavSongModel.fromMap(x as Map<String, dynamic>),
+        ),
+      ),
     );
   }
 
@@ -53,25 +65,26 @@ class UserModel {
 
   @override
   String toString() {
-    return 'UserModel(name: $name, email: $email, password: $password, token: $token)';
+    return 'UserModel(name: $name, email: $email, id: $id, token: $token, favorites: $favorites)';
   }
 
   @override
   bool operator ==(covariant UserModel other) {
     if (identical(this, other)) return true;
-  
-    return 
-      other.name == name &&
-      other.email == email &&
-      other.password == password &&
-      other.token == token;
+
+    return other.name == name &&
+        other.email == email &&
+        other.id == id &&
+        other.token == token &&
+        listEquals(other.favorites, favorites);
   }
 
   @override
   int get hashCode {
     return name.hashCode ^
-      email.hashCode ^
-      password.hashCode ^
-      token.hashCode;
+        email.hashCode ^
+        id.hashCode ^
+        token.hashCode ^
+        favorites.hashCode;
   }
 }
